0bda2e38f4907f6f7195b548cfdff9b44ea7c26e
c.klewar <c.klewar@f5.com>
2025-01-14 15:37:42 +0100

    init

---
+++ palindrome/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let s = "A man, a plan, a canal: Panama";
+
+    fn palindrome(s: &str) -> bool {
+        let mut tmp =  s.chars().filter(|c| !c.is_whitespace()).collect::<String>();
+        tmp.retain(|c| !r#"(),".;:'"#.contains(c));
+        tmp.retain(|c| !c.is_whitespace());
+        let new = tmp.to_lowercase();
+        new.eq(&new.chars().rev().collect::<String>())
+    }
+
+    println!("Is palindrome: {}", palindrome(s));
+}
